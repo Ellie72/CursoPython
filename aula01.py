3917fe5bcc71fb269abdcf53bc65f4cc0f34c570
Ellie <61954992+Ellie72@users.noreply.github.com>
2023-06-16 19:04:49 -0300

    atualização01

--- aula01.py
+++ aula01.py
@@ -1,3 +1,21 @@
 print("****************************")
 print("Bem vindo ao curso da Alura")
 print("****************************")
+
+numero_secreto=42
+chute_str = input("Digite o seu chute: ")
+print("Você digitou",chute_str)
+chute= int(chute_str)
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
+    print("Você acertou!")
+else:
+    if(maior):
+        print("Você errou! O seu chute foi maior do que o numero secreto")
+    elif(menor):
+        print("Você errou! O seu chute foi menor do que o numero secreto")
+
+print("Fim de jogo")
